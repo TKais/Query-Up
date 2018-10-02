a214ee872026f005b358d8a84262409b11dd4cb5
TKais <TiffanyTKaiser@gmail.com>
2018-10-01 18:03:21 -0700

    feat: First attempt at getting StatusBar to work

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { Constants } from 'expo';
 import DeckList from './components/DeckList';
 
 export default class App extends React.Component {
+  generateStatusBar() {
+    return (
+      <StatusBar barStyle = "dark-content" hidden = {false}/>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        { this.generateStatusBar() }
         <Text>This is working!</Text>
         <DeckList />
       </View>
