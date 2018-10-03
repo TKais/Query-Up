2bbc819723ac609e738572d72b325b39efa2268a
TKais <TiffanyTKaiser@gmail.com>
2018-10-02 22:09:34 -0700

    feat: Extract styled components to assets folder

---
+++ assets/styles/App.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledView = styled.View`
+  backgroundColor: #AAE0DC;
+  flex: 1;
+`
+
+export const StatusView = styled.View`
+  backgroundColor: #71C9C8;
+  justify-content: flex-end;
+  align-items: center;
+  height: 60;
+`
+
+export const StatusText = styled.Text`
+  color: #FFFFFF;
+  font-weight: 700;
+`
