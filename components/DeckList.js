bf534003c10ff52e120ccb1321eb7075eee08fbc
TKais <TiffanyTKaiser@gmail.com>
2018-10-03 20:36:51 -0700

    feat: Add Deck component to DeckList

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { StyledView } from '../assets/styles/decklist-styles';
+import Deck from './Deck';
 
 class DeckList extends React.Component {
 	render() {
