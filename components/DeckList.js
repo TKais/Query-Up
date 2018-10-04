3f67a880dd1c437887f5233c788c9c10fbc6b685
TKais <TiffanyTKaiser@gmail.com>
2018-10-03 20:14:30 -0700

    fix: Add new style reference

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { StyledView } from '../assets/styles/DeckList';
+import { StyledView } from '../assets/styles/decklist-styles';
 
 class DeckList extends React.Component {
 	render() {
