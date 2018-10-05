67727096dedcfcb526bff9458ef6446a7930f935
TKais <TiffanyTKaiser@gmail.com>
2018-10-05 00:39:16 -0700

    feat: Start building out Deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class Deck extends React.Component {
 	render() {
 		return (
-			<View>
-			    <Text>Deck View</Text>
-			</View>
+			<Button title={this.props.name} />
 		);
 	}
 }
