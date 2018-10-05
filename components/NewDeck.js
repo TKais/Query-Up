82d3d2dcf47c49f40f26f7e38a4940f674394189
TKais <TiffanyTKaiser@gmail.com>
2018-10-04 21:08:33 -0700

    feat: Add header to NewDeck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 class NewDeck extends React.Component {
 	render() {
 		return (
 			<View>
 			    <Text>NewDeck View</Text>
+			    <Text>What is the title of your new deck?</Text>
 			</View>
 		);
 	}
 }
 
-export default NewDeck;
+export default connect()(NewDeck);
