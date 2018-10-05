2e4477fbdcd70d5cac3d62836b1e507f600ad33f
TKais <TiffanyTKaiser@gmail.com>
2018-10-05 04:19:47 -0700

    feat: Render new Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 class Deck extends React.Component {
+	handlePress = () => {
+		// ok
+	}
+	
 	render() {
 		return (
-			<Button title={this.props.name} />
+			<Button title={this.props.title} onPress={ this.handlePress } />
 		);
 	}
 }
