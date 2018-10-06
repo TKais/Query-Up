117bb57fecbb5c472924c3f98e8acc9246d0807b
TKais <TiffanyTKaiser@gmail.com>
2018-10-06 15:52:39 -0700

    feat: Add AsyncStorage for persistence

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, Keyboard } from 'react-native';
+import { View, Text, Keyboard, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import { StyledView, StyledInput, StyledButton, ButtonText } from '../assets/styles/new-deck';
 import { createDeck } from '../actions/decks';
 import { NavigationActions } from 'react-navigation';
+import { DECK_KEY } from '../utils/storage';
 
 class NewDeck extends React.Component {
 	constructor(props) {
@@ -20,11 +21,17 @@ class NewDeck extends React.Component {
 	}
 
 	handlePress = () => {
-		this.props.dispatch( createDeck(this.state.textValue) );
-		Keyboard.dismiss();
-		this.setState({
-			textValue: ''
-		});
+		const decks = this.props.decks;
+		const value = this.state.textValue;
+
+		if ( value.length > 0 ) {
+			this.props.dispatch( createDeck(value) );
+			AsyncStorage.setItem( DECK_KEY, JSON.stringify({ [value]: { title: value, questions: [], } }))
+			    .then( Keyboard.dismiss() )
+			    .then( this.setState({ textValue: '' }) );
+	    } else if ( Object.keys(decks).length > 0 && !Object.keys(decks).includes(value) ) {
+		    return new Error( 'This deck title already exists. Please choose a new deck title.' ) ;
+		}
 		// this.props.navigation.dispatch(NavigationActions.back({key: 'DeckList'}));
 	}
 
@@ -41,4 +48,10 @@ class NewDeck extends React.Component {
 	}
 }
 
-export default connect()(NewDeck);
+function mapStateToProps({ decks }) {
+	return {
+		decks
+	}
+}
+
+export default connect(mapStateToProps)(NewDeck);
