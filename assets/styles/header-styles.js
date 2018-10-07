3d6f1fdbfebf3a04309a9b2c85bad74446a0d4f3
TKais <TiffanyTKaiser@gmail.com>
2018-10-07 12:10:06 -0700

    refactor: Remove text-related styles for the Header

--- assets/styles/header-styles.js
+++ assets/styles/header-styles.js
@@ -3,18 +3,11 @@ import { Constants } from 'expo';
 
 export const StatusView = styled.View`
   backgroundColor: #71C9C8;
-  justify-content: flex-end;
-  align-items: center;
   height: ${ props => props.statusBar.statusBarHeight }
-`
-
-export const StatusText = styled.Text`
-  color: #FFFFFF;
-  font-weight: 600;
 `
 
 StatusView.defaultProps = {
   statusBar: {
-    statusBarHeight: Constants.statusBarHeight + 20
+    statusBarHeight: Constants.statusBarHeight
   }
 }
