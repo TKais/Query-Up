d03f79d0178ba28e8ee48a33c45cb1fb2213d1ab
TKais <TiffanyTKaiser@gmail.com>
2018-10-09 21:17:02 -0700

    feat: Add wrapper for button node

--- assets/styles/single-deck-styles.js
+++ assets/styles/single-deck-styles.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
 
+export const StyledWrapper = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 export const StyledTouchDeck = styled.TouchableOpacity`
-  borderBottomWidth: 1;
-  borderBottomColor: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-width: 1;
+  border-color: #000000;
+  border-radius: 7;
+  elevation: 4;
+  shadow-color: grey;
+  shadow-opacity: 0.5;
+  shadow-radius: 10;
+  margin-top: 50;
+  padding-top: 35;
+  width: 75%;
 `
--- components/SingleDeck.js
+++ components/SingleDeck.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { StyledTouchDeck } from '../assets/styles/single-deck-styles';
+import { Text } from 'react-native';
+import { StyledTouchDeck, StyledWrapper } from '../assets/styles/single-deck-styles';
 
 class SingleDeck extends React.Component {
-	handlePress = () => {
-		// comment
-	}
+  handlePress = () => {
+    // comment
+  }
 	
-	render() {
-		return (
-			<StyledTouchDeck key={this.props.title}>
-	            <Text>{this.props.title}</Text>
-	        </StyledTouchDeck>
-		);
-	}
+  render() {
+    return (
+      <StyledWrapper>
+        <StyledTouchDeck key={this.props.title}>
+          <Text>{this.props.title}</Text>
+        </StyledTouchDeck>
+      </StyledWrapper>
+    );
+  }
 }
 
 export default SingleDeck;
