19bc42ebfbc175048e6c6a5265dac62032b7ffbe
TKais <TiffanyTKaiser@gmail.com>
2018-10-10 21:20:32 -0700

    feat: Add a custom back button with a new route

--- components/Deck.js
+++ components/Deck.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { HeaderBackButton } from 'react-navigation';
 
 class Deck extends React.Component {
   static navigationOptions = ({ navigation }) => {
     const deckName = navigation.state.params.deckName;
+
     return {
-      title: deckName
+      title: deckName,
+      headerLeft: (<HeaderBackButton onPress={()=>{navigation.navigate('Decks')}}/>),
     }
   };
 
