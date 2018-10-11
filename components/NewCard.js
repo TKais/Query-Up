b19f5789d9b84a7e9f6561eb6a4fb099189b04aa
TKais <TiffanyTKaiser@gmail.com>
2018-10-10 23:26:16 -0700

    refactor: Rename NewQuestion to NewCard

---
+++ components/NewCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class NewCard extends React.Component {
+	render() {
+		return (
+			<View>
+			    <Text>New Card View</Text>
+			</View>
+		);
+	}
+}
+
+export default NewCard;
