00ccac764db5751943d091971e8abd7ede728829
TKais <TiffanyTKaiser@gmail.com>
2018-10-12 07:38:29 -0700

    refactor: Remove common utilities from NewDeck styles and move to common

--- assets/styles/new-deck-styles.js
+++ assets/styles/new-deck-styles.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
-export const StyledView = styled.View`
-  background-color: #AAE0DC;
-  flex: 1;
-`
-
 export const StyledInput = styled.TextInput`
   height: 40; 
   border-color: #717171;
@@ -12,21 +7,4 @@ export const StyledInput = styled.TextInput`
   width: 75%;
   align-self: center;
   background-color: #FFFFFF;
-`
-
-export const StyledButton = styled.TouchableOpacity`
-  border-color: #717171;
-  border-width: 1;
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
-  width: 50%;
-  align-self: center;
-  margin-top: 10;
-  border-radius: 5;
-`
-
-export const ButtonText = styled.Text`
-  text-align: center;
 `
