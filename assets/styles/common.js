a8331f30bcb50b31e8500760195557d772b9fbc1
TKais <TiffanyTKaiser@gmail.com>
2018-10-11 22:57:40 -0700

    feat: Add a style module with common styles used across components

---
+++ assets/styles/common.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledView = styled.View`
+  background-color: #AAE0DC;
+  flex: 1;
+`
+
+export const StyledButton = styled.TouchableOpacity`
+  border-color: #717171;
+  border-width: 1;
+  padding-top: 10;
+  padding-bottom: 10;
+  padding-left: 10;
+  padding-right: 10;
+  width: 50%;
+  align-self: center;
+  margin-top: 10;
+  border-radius: 5;
+`
+
+export const ButtonText = styled.Text`
+  text-align: center;
+`
