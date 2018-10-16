7bd82eb42de7bbaef02b4d583cefa995fb1d0961
TKais <TiffanyTKaiser@gmail.com>
2018-10-15 19:13:59 -0700

    feat: Add a reducer to delete a deck

--- reducers/decks.js
+++ reducers/decks.js
@@ -1,20 +1,24 @@
-import { GET_DECKS, CREATE_DECK } from '../actions/decks';
+import { GET_DECKS, CREATE_DECK, DELETE_DECK } from '../actions/decks';
 
 export function decks( state = {}, action ) {
 	switch( action.type ) {
 		case GET_DECKS:
-		    return {
-		    	...state,
-		    	...action.decks,
-		    }
+	    return {
+	    	...state,
+	    	...action.decks,
+	    }
 		case CREATE_DECK:
-		    return {
-		    	...state,
-		    	[action.deckName]: {
-		    		title: action.deckName,
-		    		cards: [],
-		    	}
-		    }
+	    return {
+	    	...state,
+	    	[action.deckName]: {
+	    		title: action.deckName,
+	    		cards: [],
+	    	}
+	    }
+		case DELETE_DECK:
+		  const newState = Object.assign({}, state);
+		  delete newState[action.deckName];
+		  return newState;
 		default:
 		    return state;
 	}
