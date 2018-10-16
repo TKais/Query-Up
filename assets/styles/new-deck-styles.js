4ac9a815dce93752e016cd6d05a22b63466f092f
TKais <TiffanyTKaiser@gmail.com>
2018-10-15 19:49:27 -0700

    feat: Remove StyledInput from NewDeck styles

--- assets/styles/new-deck-styles.js
+++ assets/styles/new-deck-styles.js
@@ -1,10 +1,1 @@
 import styled from 'styled-components';
-
-export const StyledInput = styled.TextInput`
-  height: 40; 
-  border-color: #717171;
-  border-width: 1;
-  width: 75%;
-  align-self: center;
-  background-color: #FFFFFF;
-`
