606f0df3e952c52de8a81484d2a8a77987f22193
TKais <TiffanyTKaiser@gmail.com>
2018-10-17 00:42:35 -0700

    feat: Render quiz questions and answers + hook up event listeners

--- components/QuizCard.js
+++ components/QuizCard.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Text } from 'react-native';
 import { StyledView, StyledButton, ButtonText } from '../assets/styles/common';
 
-class QuizCard extends React.Component {
-  render() {
-    return (
-      <StyledView>
-        <Text>{this.props.question}</Text>
-        <StyledButton>
-          <ButtonText>Answer</ButtonText>
-        </StyledButton>
-        <StyledButton>
-          <ButtonText>Correct</ButtonText>
-        </StyledButton>
-        <StyledButton>
-          <ButtonText>Incorrect</ButtonText>
-        </StyledButton>
-      </StyledView>
-    );
-  }
+function QuizCard(props) {
+  return (
+    <StyledView>
+      <Text>{props.question}</Text>
+      <StyledButton>
+        <ButtonText>Answer</ButtonText>
+      </StyledButton>
+      <StyledButton onPress={props.onCorrectAnswer}>
+        <ButtonText>Correct</ButtonText>
+      </StyledButton>
+      <StyledButton onPress={props.onIncorrectAnswer}>
+        <ButtonText>Incorrect</ButtonText>
+      </StyledButton>
+    </StyledView>
+  );
 }
 
 export default QuizCard;
