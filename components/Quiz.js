789a5c765aec4c0cbe48ce19dc2db4bb2342fb6b
TKais <TiffanyTKaiser@gmail.com>
2018-10-16 20:15:10 -0700

    feat: Add state to render proper cards:

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { Text } from 'react-native';
 import { StyledView } from '../assets/styles/common';
 import { connect } from 'react-redux';
+import QuizCard from './QuizCard';
 
 class Quiz extends React.Component {
+  state = {
+    cardIndex: 0
+  }
+
   generateQuiz = () => {
     const decks = this.props.decks;
     const deckName = this.props.navigation.state.params.deckName;
     
-    return decks[deckName].cards.length > 0 ? this.showCards(decks[deckName].cards) : this.showError(deckName);
+    return decks[deckName].cards.length > 0 ? this.createCard(decks[deckName].cards) : this.showError(deckName);
   }
 
-  showCards = (cards) => {
-    console.log(cards);
+  createCard = (cards) => {
+    const cardIndex = this.state.cardIndex;
+
+    if(cardIndex < cards.length - 1) {
+      return (
+        <QuizCard question={cards[cardIndex].Question} answer={cards[cardIndex].Answer} />
+      );
+    }
   }
 
   showError = (deckName) => {
