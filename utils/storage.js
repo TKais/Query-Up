c0b7e765f053cb3bae38cafbe74c85f786fd34fb
TKais <TiffanyTKaiser@gmail.com>
2018-10-17 23:23:03 -0700

    feat: Add utility functions for AsyncStorage

--- utils/storage.js
+++ utils/storage.js
@@ -1,1 +1,15 @@
+import { AsyncStorage } from 'react-native';
+
 export const DECK_KEY = 'QueryUp:deck';
+
+export const getAsyncDecks = () => {
+  return AsyncStorage.getItem( DECK_KEY )
+}
+
+export const setAsyncDeck = ( deck ) => {
+  return AsyncStorage.setItem(DECK_KEY, JSON.stringify( deck ))
+}
+
+export const addAsyncDeck = ( deck ) => {
+  return AsyncStorage.mergeItem(DECK_KEY, JSON.stringify( deck ))
+}
