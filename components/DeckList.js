1e86b6db52f8b7a2eaad49e98ca59f86094d9fec
TKais <TiffanyTKaiser@gmail.com>
2018-10-17 23:22:18 -0700

    feat: Use AsyncStorage to persist deck list

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { View, Text, FlatList, AsyncStorage } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import { getDecks } from '../actions/decks';
 import { StyledView } from '../assets/styles/common';
 import SingleDeck from './SingleDeck';
-import { DECK_KEY } from '../utils/storage';
+import { getAsyncDecks } from '../utils/storage';
 
 class DeckList extends React.Component {
+	componentDidMount() {
+		getAsyncDecks()
+		  .then((result) => JSON.parse(result))
+		  .then((result) => this.props.dispatch(getDecks(result)))
+		  .then(() => this.setState({ loading: false }));
+	}
 
 	generateDeckList = () => {
-		const deckNames = Object.keys( this.props.decks );
 		const deckArray = Object.values(this.props.decks);
+		console.log('???????????--->', deckArray);
 
 		return (
 			<FlatList
 				data={ deckArray }
-				renderItem={ ({ item, index }) => <SingleDeck title={item.title} cardLength={item.cards.length} navigation={this.props.navigation} /> }
+				renderItem={ ({ item, index }) => <SingleDeck title={item.title} cardLength={item.questions.length} navigation={this.props.navigation} /> }
 				showsVerticalScrollIndicator={false}
 				keyExtractor={ item => item.title }
 			/>
