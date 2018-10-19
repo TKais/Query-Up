6967316b8ccfd34d280828826585499d2558b406
TKais <TiffanyTKaiser@gmail.com>
2018-10-18 20:18:34 -0700

    refactor: Delete key in AsyncStorage

--- utils/storage.js
+++ utils/storage.js
@@ -6,14 +6,16 @@ export const getAsyncDecks = () => {
   return AsyncStorage.getItem( DECK_KEY )
 }
 
-export const setAsyncDeck = ( deck ) => {
-  return AsyncStorage.setItem(DECK_KEY, JSON.stringify( deck ))
-}
-
 export const addAsyncDeck = ( deck ) => {
   return AsyncStorage.mergeItem(DECK_KEY, JSON.stringify( deck ))
 }
 
 export const removeAsyncDeck = ( deck ) => {
-  return AsyncStorage.removeItem(deck)
+  return AsyncStorage.getItem(DECK_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[deck] = undefined
+      delete data[deck]
+      AsyncStorage.setItem(DECK_KEY, JSON.stringify(data))
+    })
 }
