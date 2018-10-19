0c94bceeaea0a3f20b0d7e0e0a954e9510275fcb
TKais <TiffanyTKaiser@gmail.com>
2018-10-18 20:29:59 -0700

    feat: Use AsyncStorage to persist cards

--- components/AddCard.js
+++ components/AddCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { createCard } from '../actions/decks';
+import { addAsyncCard } from '../utils/storage';
 import { StyledView, StyledButton, ButtonText, StyledInput } from '../assets/styles/common';
 
 class AddCard extends React.Component {
@@ -24,12 +25,11 @@ class AddCard extends React.Component {
 
   handleAddCard = () => {
     const deckName = this.props.navigation.state.params.deckName;
-    this.props.dispatch(createCard(deckName, { Question: this.state.cardQuestion, Answer: this.state.cardAnswer}));
-    this.setState({
-      cardQuestion: '',
-      cardAnswer: '',
-    });
-    this.props.navigation.navigate('Decks');
+    const card = { Question: this.state.cardQuestion, Answer: this.state.cardAnswer};
+    return addAsyncCard(deckName, card)
+      .then(this.props.dispatch(createCard(deckName, card)))
+      .then(this.setState({ cardQuestion: '', cardAnswer: '' }))
+      .then(this.props.navigation.navigate('Decks'));
   }
 
   render() {
