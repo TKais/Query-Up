2317d16c1801252c7bceff39d2dad97d31355401
TKais <TiffanyTKaiser@gmail.com>
2018-10-22 20:57:25 -0700

    fix: Write a workaround for the fact that back-visibility doesn't work in Android

--- components/QuizCard.js
+++ components/QuizCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Animated, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Animated, StyleSheet, Dimensions, Platform } from 'react-native';
 import { StyledView, StyledButton, ButtonText } from '../assets/styles/common';
 import { StyledNumberText } from '../assets/styles/quiz-card-styles';
 
@@ -13,15 +13,16 @@ class QuizCard extends React.Component {
     this.value = 0;
     this.animatedValue.addListener(({ value }) => {
       this.value = value;
-    })
+    });
+
     this.frontInterpolate = this.animatedValue.interpolate({
       inputRange: [0, 180],
       outputRange: ['0deg', '180deg'],
-    })
+    });
     this.backInterpolate = this.animatedValue.interpolate({
       inputRange: [0, 180],
       outputRange: ['180deg', '360deg']
-    })
+    });
   }
 
   showQuestionAndAnswer = () => {
@@ -58,7 +59,7 @@ class QuizCard extends React.Component {
     );
   }
 
-  render() {
+  renderStyles = (side) => {
     const frontAnimatedStyle = {
       transform: [
         { rotateY: this.frontInterpolate}
@@ -69,14 +70,43 @@ class QuizCard extends React.Component {
         { rotateY: this.backInterpolate }
       ]
     }
+
+    if(Platform.OS === 'ios') {
+      return this.renderIOSStyles(side, frontAnimatedStyle, backAnimatedStyle);
+    } else {
+      return this.renderAndroidStyles(side, frontAnimatedStyle, backAnimatedStyle);
+    }
+  }
+
+  renderIOSStyles = (side, frontAnimatedStyle, backAnimatedStyle) => {
+    if(side === 'front') {
+      return [styles.flipCard, frontAnimatedStyle];
+    } else {
+      return [backAnimatedStyle, styles.flipCard, styles.flipCardBack];
+    }
+  }
+
+  renderAndroidStyles = (side, frontAnimatedStyle, backAnimatedStyle) => {
+    const showOrHideBack = this.state.answerIsVisible ? styles.androidShow : styles.androidHide;
+    const showOrHideFront = this.state.answerIsVisible ? styles.androidHide : styles.androidShow;
+
+
+    if(side === 'front') {
+      return [styles.flipCard, frontAnimatedStyle, showOrHideFront];
+    } else {
+      return [backAnimatedStyle, styles.flipCard, styles.flipCardBack, showOrHideBack];
+    }
+  }
+
+  render() {
     return (
       <StyledView>
         <StyledNumberText>{ `${this.props.cardIndex}/${this.props.cards.length}` }</StyledNumberText>
-        <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+        <Animated.View style={this.renderStyles('front')}>
         <Text>{this.props.question}</Text>
         { this.renderButtonContainer() }
         </Animated.View>
-        <Animated.View style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
+        <Animated.View style={this.renderStyles('back')}>
           <Text>{this.props.answer}</Text>
           { this.renderButtonContainer() }
         </Animated.View>
@@ -86,6 +116,12 @@ class QuizCard extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  androidShow: {
+    opacity: 1,
+  },
+  androidHide: {
+    opacity: 0,
+  },
   flipCard: {
     width: Dimensions.get('window').width,
     backfaceVisibility: 'hidden',
