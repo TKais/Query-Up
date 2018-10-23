0081d7dac909c2bf478e6afe2e972a4c5a854995
TKais <TiffanyTKaiser@gmail.com>
2018-10-22 23:11:04 -0700

    feat: Finish color generator algorithm

--- utils/styles.js
+++ utils/styles.js
@@ -1,6 +1,8 @@
-export const setDeckColors = (deckLength) => {
-  const colors = ['#A8D8D8', '#71C9C8', '#FDB2B3', '#FED0B7', '#F9E3B7', '#F8EEBA'];
-  console.log('DECK LENGTH--->', deckLength);
-
-  return 'red';
+export const setDeckColors = (deckIndex) => {
+  const colors = ['#71C9C8', '#FDB2B3', '#FED0B7', '#F9E3B7', '#F8EEBA'];
+  if(deckIndex < colors.length) {
+    return colors[deckIndex];
+  } else {
+    return colors[deckIndex % colors.length];
+  }
 }
