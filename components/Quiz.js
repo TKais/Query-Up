2b396868dd6a15abcf28951e618d40375b64c2ae
TKais <TiffanyTKaiser@gmail.com>
2018-10-22 21:24:00 -0700

    fix: Add header navigation on Android for the Quiz view

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 import { StyledView } from '../assets/styles/common';
 import { connect } from 'react-redux';
 import QuizCard from './QuizCard';
@@ -11,6 +12,17 @@ class Quiz extends React.Component {
     correct: 0,
     incorrect: 0
   }
+
+  static navigationOptions = ({ navigation }) => {
+    const deckName = navigation.state.params.deckName;
+
+    if(Platform.OS === 'android') {
+      return {
+        title: deckName,
+        headerLeft: <Ionicons name={ 'md-arrow-back' } size={30} style={{ marginTop: -5, marginLeft: 10 }} onPress={() => navigation.navigate('Deck')} title={deckName} color="#FFFFFF" />,
+      }
+    }
+  };
 
   generateQuiz = () => {
     const decks = this.props.decks;
