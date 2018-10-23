acd541b4ea2957ecdff485bfea5aa56305fc8b9c
TKais <TiffanyTKaiser@gmail.com>
2018-10-22 22:11:50 -0700

    feat: Set up color util

--- utils/styles.js
+++ utils/styles.js
@@ -1,3 +1,6 @@
-export const setCardColors = () => {
+export const setDeckColors = (deckLength) => {
+  const colors = ['#A8D8D8', '#71C9C8', '#FDB2B3', '#FED0B7', '#F9E3B7', '#F8EEBA'];
+  console.log('DECK LENGTH--->', deckLength);
+
   return 'red';
 }
