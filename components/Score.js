394b2ce2769271ef1778769f7d705ef7ff10b637
TKais <TiffanyTKaiser@gmail.com>
2018-10-25 20:08:10 -0700

    refactor: Change button colors on Score component

--- components/Score.js
+++ components/Score.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { StyledView, StyledButton, ButtonText } from '../assets/styles/common';
+import { colorPalette } from '../utils/styles';
 
 class Score extends React.Component {
   startQuiz = () => {
@@ -16,10 +17,10 @@ class Score extends React.Component {
       <StyledView>
         <Text>Correct: {this.props.numberCorrect}</Text>
         <Text>Incorrect: {this.props.numberIncorrect}</Text>
-        <StyledButton onPress={this.startQuiz} activeOpacity={0.8} theme={{ buttonColor: '#66A266' }}>
+        <StyledButton onPress={this.startQuiz} activeOpacity={0.8} theme={{ buttonColor: colorPalette.peach }}>
           <ButtonText>Restart Quiz</ButtonText>
         </StyledButton>
-        <StyledButton onPress={this.routeToDeck} activeOpacity={0.8} theme={{ buttonColor: '#66A266' }}>
+        <StyledButton onPress={this.routeToDeck} activeOpacity={0.8} theme={{ buttonColor: colorPalette.coral }}>
           <ButtonText>Back to Deck</ButtonText>
         </StyledButton>
       </StyledView>
